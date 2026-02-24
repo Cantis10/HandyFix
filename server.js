d72e7795cb7f164b614aecf263cc00d90f859972
Cantis <148445784+Cantis10@users.noreply.github.com>
2026-02-24 11:34:09 +0000

    push!

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Express!');
+});
+
+app.listen(PORT, () => {
+  c
+
+
+onsole.log(`Server running on http://localhost:${PORT}`);
+});
