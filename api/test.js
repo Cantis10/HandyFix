d895737c352e78572a428f06ecface9150f33866
Cantis <148445784+Cantis10@users.noreply.github.com>
2026-02-24 14:11:49 +0000

    punsh

--- api/test.js
+++ api/test.js
@@ -1,3 +1,12 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello, this is JSON!" });
-}
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: "Hello JSON from Express + Vercel!" });
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
