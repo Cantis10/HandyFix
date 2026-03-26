1780976facef871edf673de44e49ddb60256fbb5
Cantis <148445784+Cantis10@users.noreply.github.com>
2026-03-26 05:06:32 +0000

    post

--- routes/pages/contractPages.js
+++ routes/pages/contractPages.js
@@ -3,15 +3,27 @@ const app = express.Router();
 const path = require('path');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/contractor/home.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/contractor/contractor_dashboard.html'));
 });
 
-app.get('/job', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/contractor/job.html'));
+app.get('/calendar', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/contractor/contractor_endar.html'));
+});
+
+app.get('/bookings', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/contractor/contractor_bookings.html'));
+});
+
+app.get('/messages', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/contractor/contractor_messages.html'));
 });
 
 app.get('/profile', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/contractor/profile.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/contractor/contractor_profile.html'));
+});
+
+app.get('/settings', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/contractor/contractor_settings.html'));
 });
 
 
