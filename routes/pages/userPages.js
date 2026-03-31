69ff605965ed8447878e5a186e467396d717b49e
Cantis10 <propertyofcantis@gmail.com>
2026-03-31 18:25:28 +0800

    rename

--- routes/pages/userPages.js
+++ routes/pages/userPages.js
@@ -1,40 +1,47 @@
 const express = require("express");
 const app = express.Router();
-const path = require('path');
-const { requireAuth } = require("../token"); 
-app.get('/', requireAuth('user'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/user/home.html'));
+const path = require("path");
+const { requireAuth } = require("../token");
+app.get("/", requireAuth("user"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/user/home.html"));
 });
 
-app.get('/booking', requireAuth('user'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/user/bookings.html'))
+app.get("/booking", requireAuth("user"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/user/bookings.html"));
 });
 
-app.get('/messages', requireAuth('user'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/user/messages.html'))
+app.get("/messages", requireAuth("user"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/user/messages.html"));
 });
 
-app.get('/profile', requireAuth('user'),   (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/user/profile.html'))
+app.get("/profile", requireAuth("user"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/user/profile.html"));
 });
 
-app.get('/settings', requireAuth('user'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/user/settings.html'))
+app.get("/settings", requireAuth("user"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/user/settings.html"));
 });
 
-app.get('/support', requireAuth('user'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/user/support.html'))
+app.get("/support", requireAuth("user"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/user/support.html"));
 });
 
+app.get("/repairs", requireAuth("user"), (req, res) => {
+  const query = req.query;
 
+  if (query.service || query.sub) {
+    const service = query.sub.toLowerCase().trim().replace(/\s+/g, "_");
+    console.log(service);
+    return res.sendFile(
+      path.join(__dirname, `../../frontend/user/${service}.html`),
+    );
+  }
 
-app.get('/repairs', requireAuth('user'), (req, res) => {
-  res.sendFile(path.join(__dirname, `../../frontend/user/repair.html`))
+  res.sendFile(path.join(__dirname, `../../frontend/user/repair.html`));
 });
 
-app.get('/initiateRepair', requireAuth('user'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/user/initiateRepair.html'))
+app.get("/initiateRepair", requireAuth("user"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/user/initiateRepair.html"));
 });
-
 
 module.exports = app;
