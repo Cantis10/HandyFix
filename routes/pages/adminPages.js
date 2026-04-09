048fdea2f2656c35651b4b677198f53b6a5b6291
Cantis10 <propertyofcantis@gmail.com>
2026-04-09 19:52:58 +0800

    no

--- routes/pages/adminPages.js
+++ routes/pages/adminPages.js
@@ -1,42 +1,53 @@
 const express = require("express");
 const app = express.Router();
-const path = require('path');
-const { requireAuth } = require("../token"); 
-app.get('/', requireAuth('admin'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/Admin_Dashboard.html'));
+const path = require("path");
+const { requireAuth } = require("../token");
+app.get("/", requireAuth("admin"), (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../../frontend/admin/Admin_Dashboard.html"),
+  );
 });
 
-app.get('/service', requireAuth('admin'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/Service_Requests.html'));
+app.get("/dashboard", requireAuth("admin"), (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../../frontend/admin/Admin_Dashboard.html"),
+  );
 });
 
-app.get('/contractor', requireAuth('admin'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/Contractor.html'));
+app.get("/service", requireAuth("admin"), (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../../frontend/admin/Service_Requests.html"),
+  );
 });
 
-app.get('/categories', requireAuth('admin'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/Edit_Categories.html'));
+app.get("/contractor", requireAuth("admin"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/admin/Contractor.html"));
 });
 
-app.get('/customers', requireAuth('admin'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/Customers.html'));
+app.get("/categories", requireAuth("admin"), (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../../frontend/admin/Edit_Categories.html"),
+  );
 });
 
-app.get('/assignment', requireAuth('admin'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/Assignment.html'));
+app.get("/customers", requireAuth("admin"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/admin/Customers.html"));
 });
 
-
-app.get('/messages', requireAuth('admin'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/Messages.html'));
+app.get("/assignment", requireAuth("admin"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/admin/Assignment.html"));
 });
 
-app.get('/reports', requireAuth('admin'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/Reports.html'));
+app.get("/messages", requireAuth("admin"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/admin/Messages.html"));
 });
 
-app.get('/settings', requireAuth('admin'), (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/Settings.html'));
+app.get("/reports", requireAuth("admin"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/admin/Reports.html"));
+});
+
+app.get("/settings", requireAuth("admin"), (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/admin/Settings.html"));
 });
 
 module.exports = app;
